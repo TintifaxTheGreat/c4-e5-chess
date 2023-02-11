849593f31d392249ceda44c4bf84cc2b535f1467
Eugen Lindorfer <eugen.lindorfer@lindy.net>
2023-02-11 17:55:44 +0100

    Change: Continued on implementation

--- src/engine/game.rs
+++ src/engine/game.rs
@@ -1,6 +1,6 @@
-use super::constants::*;
+use super::{constants::*, store::Item, store::Store};
 use chess::{Board, ChessMove, MoveGen};
-use std::{cell::Cell, str::FromStr, time::Duration};
+use std::{cell::Cell, mem, str::FromStr, time::Duration};
 
 #[derive(Debug)]
 pub enum ChessError {
@@ -8,11 +8,19 @@ pub enum ChessError {
     FenNotValid,
 }
 
+pub struct MoveValue {
+    // TODO check if we really need this
+    pub m: ChessMove,
+    pub v: i32,
+}
+
+pub type MoveValueOption = Option<MoveValue>;
 pub type MoveResult = Result<ChessMove, ChessError>;
-struct Game {
+
+pub struct Game {
     max_depth: u16,
     inc_quiet_depth: u16,
-    board: Board,
+    pub board: Board,
     playing: Cell<bool>,
     move_time: Duration,
     //TODO board_history:
@@ -47,6 +55,7 @@ impl Game {
     }
 
     pub fn find_move(&mut self) -> MoveResult {
+        let mut store: Store = Store::new();
         let mut result: Board = Board::default();
         let alpha = MIN_INT;
         let beta = MAX_INT;
@@ -59,10 +68,24 @@ impl Game {
         }
 
         while current_depth <= self.max_depth {
-            let mut prior_values: Vec<(&ChessMove, i32)>;
-
+            let mut prior_values: Vec<(ChessMove, i32)> = Vec::new();
             for m in &mut moves {
-                &self.board.make_move(m, &mut result); //TODO
+                let mut bresult = mem::MaybeUninit::<Board>::uninit();
+                unsafe {
+                    &self.board.make_move(m, &mut *bresult.as_mut_ptr());
+                    //let hop = store.get(current_depth, &*bresult.as_ptr());
+                    match store.get(current_depth, &*bresult.as_ptr()) {
+                        Some((mm, v)) => prior_values.push((mm, -v)),
+                        None => {
+                            let foo = &mut self.negamax(current_depth, -beta, -alpha, false, false);
+
+                            //priorValues[move] *= -1
+                        }
+                    }
+
+                    //result += MoveGen::movegen_perft_test(&*bresult.as_ptr(), depth - 1);
+                }
+                //&self.board.make_move(m, &mut result); //TODO
             }
         }
 
@@ -70,6 +93,7 @@ impl Game {
     }
 
     fn negamax(
+        &mut self,
         depth: u16,
         alpha: i32,
         beta: i32,
