7dd82923a430754c11b0126ea9edbad9f2c167f7
Eugen Lindorfer <eugen.lindorfer@lindy.net>
2023-03-10 20:57:21 +0100

    Add: one more position unit test

--- src/engine/game.rs
+++ src/engine/game.rs
@@ -271,7 +271,7 @@ mod tests {
             Some(m) => assert_eq!(m.to_string(), "e5e1"),
             None => panic!("No move found"),
         }
-        /*
+        
         // Test 7
         let mut g = Game::new(
             "3q1rk1/4bp1p/1n2P2Q/1p1p1p2/6r1/Pp2R2N/1B1P2PP/7K w - - 1 0".to_string(),
@@ -282,6 +282,6 @@ mod tests {
             Some(m) => assert_eq!(m.to_string(), "h3g5"),
             None => panic!("No move found"),
         }
-        */
+        
     }
 }
