679bb5e69131c33a764b927d99c7b83f5bdd846e
Eugen Lindorfer <eugen.lindorfer@lindy.net>
2023-03-24 14:06:39 +0100

    Change: clean up code

--- src/engine/pvs.rs
+++ src/engine/pvs.rs
@@ -1,4 +1,10 @@
-use super::{constants::MATE, evaluate::evaluate, move_gen::MoveGenPrime, store::Store, types::*};
+use super::{
+    constants::{MATE, MIN_INT},
+    evaluate::evaluate,
+    move_gen::MoveGenPrime,
+    store::Store,
+    types::*,
+};
 use chess::{Board, BoardStatus, ChessMove, MoveGen};
 use std::{
     mem,
@@ -19,8 +25,7 @@ pub fn pvs(
 ) -> MoveScore {
     let mut best_move: Option<ChessMove> = None;
     let children: Vec<AnnotatedMove>;
-    let mut child_old: AnnotatedMove;
-    let mut score;
+    let mut score = MIN_INT;
     let mut value: i32;
 
     if !playing.load(Ordering::Relaxed) {
@@ -49,72 +54,69 @@ pub fn pvs(
         // return quiesce::quiesce(board, alpha, beta, playing, stop_time, node_count);
     }
 
-    let mut moves = children.iter();
-    let child_1 = moves.next().unwrap();
+    let moves = children.iter();
     let mut bresult = mem::MaybeUninit::<Board>::uninit();
-    unsafe {
-        let _ = &board.make_move(child_1.mv, &mut *bresult.as_mut_ptr());
-    }
-    unsafe {
-        score = -pvs(
-            *bresult.as_ptr(),
-            store,
-            depth - 1,
-            -beta,
-            -alpha,
-            playing,
-            node_count,
-        )
-    }
-    child_old = *child_1;
 
-    for child in &mut moves {
+    for (i, child) in &mut moves.enumerate() {
+        unsafe {
+            let _ = &board.make_move(child.mv, &mut *bresult.as_mut_ptr());
+        }
+        if i == 0 {
+            unsafe {
+                score = -pvs(
+                    *bresult.as_ptr(),
+                    store,
+                    depth - 1,
+                    -beta,
+                    -alpha,
+                    playing,
+                    node_count,
+                )
+            }
+        } else {
+            unsafe {
+                value = -pvs(
+                    *bresult.as_ptr(),
+                    store,
+                    depth - 1,
+                    -alpha - 1,
+                    -alpha,
+                    playing,
+                    node_count,
+                )
+            }
+
+            if value > score {
+                if alpha < value && value < beta && depth > 2 {
+                    unsafe {
+                        score = -pvs(
+                            *bresult.as_ptr(),
+                            store,
+                            depth - 1,
+                            -beta,
+                            -value,
+                            playing,
+                            node_count,
+                        )
+                    }
+                } else {
+                    score = value;
+                }
+            }
+        }
+
         if score >= beta {
-            best_move = Some(child_old.mv);
+            best_move = Some(child.mv);
             break;
         }
         if score > alpha {
             alpha = score;
-            best_move = Some(child_old.mv);
+            best_move = Some(child.mv);
         }
-
-        unsafe {
-            let _ = &board.make_move(child.mv, &mut *bresult.as_mut_ptr());
-        }
-        unsafe {
-            value = -pvs(
-                *bresult.as_ptr(),
-                store,
-                depth - 1,
-                -alpha - 1,
-                -alpha,
-                playing,
-                node_count,
-            )
-        }
-
-        if value > score {
-            if alpha < value && value < beta && depth > 2{
-                unsafe {
-                    score = -pvs(
-                        *bresult.as_ptr(),
-                        store,
-                        depth - 1,
-                        -beta,
-                        -value,
-                        playing,
-                        node_count,
-                    )
-                }
-            } else {
-                score = value;
-            }
-        }
-        child_old = *child;
     }
-    // TODO check why storing takes that long (use hashing?) 
+    // TODO check why storing takes that long (use hashing?)
     if best_move.is_some() {
-        store.put(depth-1, score, &board, &best_move.unwrap());
+        store.put(depth - 1, score, &board, &best_move.unwrap());
     }
     return score;
 }
