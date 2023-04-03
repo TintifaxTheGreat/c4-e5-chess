ed572735733fc430fbae8883042e1b9535b2f6bb
Eugen Lindorfer <eugen.lindorfer@lindy.net>
2023-04-03 11:41:56 +0200

    Change: repair Eigenmann test

---
+++ src/misc/helpers.rs
@@ -0,0 +1,11 @@
+use chess::{Board, ChessMove};
+use std::str::FromStr;
+
+pub fn parse_epd(epd: String) -> (String, String) {
+    let mut s = epd.split(" bm ");
+    let fen = s.next().expect("Cannot parse EPD.");
+    let best_move = s.next().expect("Cannot parse EPD.");
+    let board = Board::from_str(fen).expect("Invalid board.");
+    let mv = ChessMove::from_san(&board, best_move).expect("Invalid move.");
+    (fen.to_string(), mv.to_string())
+}
