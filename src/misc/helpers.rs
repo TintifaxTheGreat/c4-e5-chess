52a5a9d67205738e8fdb9096985d5a591f6df2eb
Eugen Lindorfer <eugen.lindorfer@lindy.net>
2025-01-11 12:51:06 +0100

    Fix: Eigenmann tests

--- src/misc/helpers.rs
+++ src/misc/helpers.rs
@@ -1,15 +1,24 @@
-use cozy_chess::{Board, Move};
+use cozy_chess::{util, Board};
 use std::str::FromStr;
 
 /// Parse EPD (Extended Position Description)
 #[allow(dead_code)]
-pub fn parse_epd(epd: String) -> (String, String) {
-    let mut s = epd.split(" bm ");
+pub fn parse_epd(epd: String) -> (String, Vec<String>, bool) {
+    let find_best_move: bool = epd.contains("bm");
+    let delimiter = if find_best_move { " bm " } else { " am " };
+    let mut s = epd.split(delimiter);
     let fen = s.next().expect("Cannot parse EPD.");
     let fen = format!("{} 0 1", fen);
+    let board = Board::from_str(&fen).expect("Invalid board.");
 
-    let best_move = s.next().expect("Cannot parse EPD.");
-    let _board = Board::from_str(&fen).expect("Invalid board.");
-    let mv = Move::from_str(best_move).expect("Invalid move.");
-    (fen.to_string(), mv.to_string())
+    let expected_moves_s = s.next().expect("Cannot parse EPD.");
+    let expected_moved: Vec<&str> = expected_moves_s.split(',').collect();
+    let mut valid_best_moves = Vec::new();
+
+    for best_move in expected_moved {
+        let best_move = best_move.trim();
+        let mv = util::parse_san_move(&board, best_move).expect("Invalid move.");
+        valid_best_moves.push(mv.to_string());
+    }
+    (fen.to_string(), valid_best_moves, find_best_move)
 }
